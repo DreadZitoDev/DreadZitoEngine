a25d701af322ac682305c9300b11f501dedfc59e
DreadZitoDev <hernanmontenegro011023@gmail.com>
2025-05-23 17:14:23 -0300

    added interactions prefabs

--- Editor/CustomHierarchyMenu.cs
+++ Editor/CustomHierarchyMenu.cs
@@ -5,47 +5,40 @@ namespace DreadZitoEngine.Runtime.Editor
 {
     public class CustomHierarchyMenu
     {
+        public const string PLAYER_SPAWN_PREFAB_PATH = "Assets/DreadZitoEngine/Prefabs/PlayerSpawn";
+        public const string TRIGGER_PREFAB_PATH = "Assets/DreadZitoEngine/Prefabs/Engine/Trigger";
+        public const string HOTSPOT_PREFAB_PATH = "Assets/DreadZitoEngine/Prefabs/InteractionSystem/Hotspot";
+        public const string CUTSCENE_CONTENT_PREFAB_PATH = "Assets/DreadZitoEngine/Prefabs/CutsceneContent";
+        public const string GAME_PREFAB_PATH = "Assets/DreadZitoEngine/Prefabs/GAME";
         
-        [MenuItem("GameObject/ROOM 502/PlayerSpawn", false, 1)]
+        [MenuItem("GameObject/DreadZitoEngine/PlayerSpawn", false, 1)]
         static void CreatePlayerSpawn()
         {
-            InstantiatePrefab(CustomEditorsDefinitions.PLAYER_SPAWN_PREFAB_PATH);
+            InstantiatePrefab(PLAYER_SPAWN_PREFAB_PATH);
         }
         
-        [MenuItem("GameObject/ROOM 502/Hotspot", false, 1)]
+        [MenuItem("GameObject/DreadZitoEngine/Hotspot", false, 1)]
         static void CreateHotspot()
         {
-            InstantiatePrefab(CustomEditorsDefinitions.HOTSPOT_PREFAB_PATH);
+            InstantiatePrefab(HOTSPOT_PREFAB_PATH);
         }
         
-        [MenuItem("GameObject/ROOM 502/Interactable", false, 1)]
-        static void CreateInteractable()
-        {
-            InstantiatePrefab(CustomEditorsDefinitions.INTERACTABLE_PREFAB_PATH);
-        }
-        
-        [MenuItem("GameObject/ROOM 502/Trigger", false, 1)]
+        [MenuItem("GameObject/DreadZitoEngine/Trigger", false, 1)]
         static void CreateTrigger()
         {
-            InstantiatePrefab(CustomEditorsDefinitions.TRIGGER_PREFAB_PATH);
-        }
-        
-        [MenuItem("GameObject/ROOM 502/EventSystem", false, 1)]
-        static void CreateEventSystem()
-        {
-            InstantiatePrefab(CustomEditorsDefinitions.EVENT_SYSTEM_PREFAB_PATH);
+            InstantiatePrefab(TRIGGER_PREFAB_PATH);
         }
 
-        [MenuItem("GameObject/ROOM 502/CutsceneContent", false, 1)]
+        [MenuItem("GameObject/DreadZitoEngine/CutsceneContent", false, 1)]
         static void CreateCutsceneContent()
         {
-            InstantiatePrefab(CustomEditorsDefinitions.CUTSCENE_CONTENT_PREFAB_PATH, false);
+            InstantiatePrefab(CUTSCENE_CONTENT_PREFAB_PATH, false);
         }
         
-        [MenuItem("GameObject/ROOM 502/GAME", false, 1)]
+        [MenuItem("GameObject/DreadZitoEngine/GAME", false, 1)]
         static void CreateGame()
         {
-            InstantiatePrefab(CustomEditorsDefinitions.GAME_PREFAB_PATH, false);
+            InstantiatePrefab(GAME_PREFAB_PATH, false);
         }
 
         static GameObject InstantiatePrefab(string prefabPath, bool autoSelect = true)
