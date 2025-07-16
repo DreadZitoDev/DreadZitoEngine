bc0f9a143f09c44576eceda8a9d5cefef3eccfb5
DreadZitoDev <hernanmontenegro011023@gmail.com>
2025-07-16 12:20:11 -0300

    combination detecting overlaps

--- Runtime/DreadZitoEngineCore.cs
+++ Runtime/DreadZitoEngineCore.cs
@@ -38,7 +38,6 @@ namespace DreadZitoEngine.Runtime
             {
                 return itemData;
             }
-            Debug.LogError($"ItemDataSO is not of type {typeof(T).Name}");
             return null;
         }
     }
